36410be94f0c69245a0d16e6ed84977b4cda868d
Talles-G <tallesgabrielduartel@gmail.com>
2024-09-03 18:02:14 -0300

    Correções

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle("light")
   //if(html.classList.contains('light')) {
   // html.classList.remove('light')
   //} else {
   //  html.classList.add('light')
   //}
-  const img = document.querySelector('#profile img')
+  const img = document.querySelector("#profile img")
+
   if(html.classList.contains('light')) {
-  img.setAttribute('src', '/assets/avatar-light.png')
+
+  img.setAttribute('src', './assets/avatar-light.png')
+
   } else {
-    img.setAttribute('src', '/assets/avatar.png')
+
+    img.setAttribute('src', './assets/avatar.png')
+
   }
 } 
